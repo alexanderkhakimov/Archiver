0013faef2a875383f0e589cbf752c01b73edd437
Alex Khakimov <alexanderkhakimov@gmail.com>
2023-10-19 12:39:39 +0300

    Реализовать класс Archiver с учетом enum Operation и класса CommandExecutor

--- src/main/java/org/myPetProject/Archiver.java
+++ src/main/java/org/myPetProject/Archiver.java
@@ -1,28 +1,44 @@
 package org.myPetProject;
 
 import org.myPetProject.command.ExitCommand;
+import org.myPetProject.exception.PathsNotFoundException;
+import org.myPetProject.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 /**
  * Hello world!
- *
  */
-public class Archiver
-{
-    public static void main( String[] args ) throws Exception
-    {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Введите полный путь файла архива:");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(br.readLine()));
-
-        System.out.println("Введите полное имя файла для архивации");
-        zipFileManager.createZip(Paths.get(br.readLine()));
-
-        new ExitCommand().execute();
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали не верный файл.");
+            }catch (PathsNotFoundException e) {
+                ConsoleHelper.writeMessage("Произошла ошибкаю Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
     }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл их архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
+
+
 }
---
+++ src/main/java/org/myPetProject/exception/WrongZipFileException.java
@@ -0,0 +1,4 @@
+package org.myPetProject.exception;
+
+public class WrongZipFileException extends Exception{
+}
