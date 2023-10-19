051dd474e50a290d9844aa235d2b69a011e08572
Alex Khakimov <alexanderkhakimov@gmail.com>
2023-10-19 13:25:40 +0300

    Добавить и реализовать вспомогательный класс FileManager который обходит корневой путь в поисках файлов в папках

---
+++ src/main/java/org/myPetProject/FileManager.java
@@ -0,0 +1,38 @@
+package org.myPetProject;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    private Path rootPath;
+    private List<Path> fileList;
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        this.fileList=new ArrayList<>();
+        collectFileList(rootPath);
+    }
+
+    private void collectFileList(Path path)throws IOException {
+        if(Files.isRegularFile(path)){
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        if(Files.isDirectory(path)){
+            try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)){
+                for(Path file : directoryStream){
+                    collectFileList(file);
+                }
+            }
+        }
+    }
+}
